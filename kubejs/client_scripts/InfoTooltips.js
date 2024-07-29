135a6ab502b15b502fc87ca916d401a3860c55f5
pieman492 <rauljrrauljr@gmail.com>
2024-07-29 16:39:08 -0500

    Config Changes

--- kubejs/client_scripts/InfoTooltips.js
+++ kubejs/client_scripts/InfoTooltips.js
@@ -4,9 +4,12 @@ let enabledMessage = "\u00a7cCrafting of this variant has been left enabled to p
 let enabledWarning = "\u00a7cDropping it in the world will result in it being converted to its parent block!"
 let quarkOverlapMessage = "\u00a7cDisabled to avoid overlap with Quark."
 let chippedTableMessage = "\u00a7aVariant block display has been disabled in EMI, but the tables still work!"
+<<<<<<< Updated upstream
 let balanceDisabledMessage = "\u00a7cCrafting disabled for balance purposes."
 let sophisticatedStorageWoodMessage = "\u00a7aWood used affects resulting color."
 let sophisticatedStorageDyeMessage = "\u00a7aCan be dyed!"
+=======
+>>>>>>> Stashed changes
 
 ItemEvents.tooltip(event => {
 	event.add('#strangejourney:chipped_table', chippedTableMessage)	
