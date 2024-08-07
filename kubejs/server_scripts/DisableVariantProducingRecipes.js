195cba8fa59ac0ec81d69a0a8819722313537d68
pieman492 <rauljrrauljr@gmail.com>
2024-08-07 10:06:51 -0500

    New variants, undisable mesh fences

--- kubejs/server_scripts/DisableVariantProducingRecipes.js
+++ kubejs/server_scripts/DisableVariantProducingRecipes.js
@@ -3,7 +3,49 @@ let disableRecipeIds = [
 	'hexerei:infused_fabric_carpet_ornate',
 	'hexerei:waxed_infused_fabric_carpet_ornate',
 	'hexerei:infused_fabric_carpet',
-	'hexerei:waxed_infused_fabric_carpet'
+	'hexerei:waxed_infused_fabric_carpet',
+	'deep_aether:roseroot_stairs',
+	'deep_aether:yagroot_stairs',
+	'deep_aether:cruderoot_stairs',
+	'deep_aether:conberry_stairs',
+	'deep_aether:sunroot_stairs',
+	'deep_aether:roseroot_slab',
+	'deep_aether:yagroot_slab',
+	'deep_aether:cruderoot_slab',
+	'deep_aether:conberry_slab',
+	'deep_aether:sunroot_slab',
+	'deep_aether:mossy_holystone_brick_stairs',
+	'deep_aether:mossy_holystone_tile_stairs',
+	'deep_aether:gilded_holystone_brick_stairs',
+	'deep_aether:gilded_holystone_tile_stairs',
+	'deep_aether:blightmoss_holystone_brick_stairs',
+	'deep_aether:blightmoss_holystone_tile_stairs',
+	'deep_aether:holystone_tile_stairs',
+	'deep_aether:big_holystone_bricks_stairs',
+	'deep_aether:cobbled_aseterite_stairs',
+	'deep_aether:aseterite_stairs',
+	'deep_aether:polished_aseterite_stairs',
+	'deep_aether:aseterite_bricks_stairs',
+	'deep_aether:raw_clorite_stairs',
+	'deep_aether:clorite_stairs',
+	'deep_aether:polished_clorite_stairs',
+	'deep_aether:aether_mud_bricks_stairs',
+	'deep_aether:mossy_holystone_brick_slab',
+	'deep_aether:mossy_holystone_tile_slab',
+	'deep_aether:gilded_holystone_brick_slab',
+	'deep_aether:gilded_holystone_tile_slab',
+	'deep_aether:blightmoss_holystone_brick_slab',
+	'deep_aether:blightmoss_holystone_tile_slab',
+	'deep_aether:holystone_tile_slab',
+	'deep_aether:big_holystone_bricks_slab',
+	'deep_aether:cobbled_aseterite_slab',
+	'deep_aether:aseterite_slab',
+	'deep_aether:polished_aseterite_slab',
+	'deep_aether:aseterite_bricks_slab',
+	'deep_aether:raw_clorite_slab',
+	'deep_aether:clorite_slab',
+	'deep_aether:polished_clorite_slab',
+	'deep_aether:aether_mud_bricks_slab'
 ]
 let disableRecipeTags = [
 	'#minecraft:stairs',
@@ -41,7 +83,8 @@ ServerEvents.tags('item', event => {
 	strippedDisableRecipeTags.forEach(tag => {
 		var taggedBlocks = Block.getTaggedIds(tag).toArray().filter(item => {
 			if( Item.of(item).getMod().equals('framedblocks') ||
-				item.toString().includes('mogmoss_rug') 
+					item.toString().includes('mogmoss_rug') ||
+					item.toString().includes('mesh_fence')
 			) {
 				return false
 			}
