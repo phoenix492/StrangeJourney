fdfd7b66021091cd2f5b68279ce9134ce0a1f854
pieman492 <rauljrrauljr@gmail.com>
2024-08-26 15:15:40 -0500

    Remove Alexs' Caves uranium from forge tags

---
+++ kubejs/server_scripts/alexs_caves.js
@@ -0,0 +1,4 @@
+ServerEvents.tags('item', event => {
+    event.remove('forge:raw_materials/uranium', 'alexscaves:uranium')
+    event.remove('forge:storage_blocks/uranium', 'alexscaves:block_of_uranium')
+})
