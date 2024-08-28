ab650591ccabf3adb54facb994e40fd6ae03ac15
pieman492 <rauljrrauljr@gmail.com>
2024-08-28 10:20:54 -0500

    Adjust Alexs Caves Tags, closes #73

--- kubejs/server_scripts/alexs_caves.js
+++ kubejs/server_scripts/alexs_caves.js
@@ -1,4 +1,13 @@
 ServerEvents.tags('item', event => {
     event.remove('forge:raw_materials/uranium', 'alexscaves:uranium')
     event.remove('forge:storage_blocks/uranium', 'alexscaves:block_of_uranium')
+    event.add('forge:ores/redstone', 'alexscaves:guanostone_redstone_ore')
+    event.add('forge:ores/coal', 'alexscaves:coprolith_coal_ore')
+})
+
+ServerEvents.tags('block', event => {
+    event.remove('forge:storage_blocks/uranium', 'alexscaves:block_of_uranium')
+    event.remove('forge:ores/uranium', 'alexscaves:radrock_uranium_ore')
+    event.add('forge:ores/redstone', 'alexscaves:guanostone_redstone_ore')
+    event.add('forge:ores/coal', 'alexscaves:coprolith_coal_ore')
 })
