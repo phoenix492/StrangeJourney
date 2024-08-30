f8420d379a0db52cd05e8740035fa74e0734815b
pieman492 <rauljrrauljr@gmail.com>
2024-08-30 16:11:54 -0500

    Makes potion removal actually work with kubejs

--- kubejs/server_scripts/mowziesmobs.js
+++ kubejs/server_scripts/mowziesmobs.js
@@ -1,3 +1,3 @@
-ServerEvents.recipes(event => {
-	event.remove({ id: 'emi:/brewing/minecraft/potion/mowziesmobs/naga_fang/minecraft/awkward/mowziesmobs/poison_resist' })
+MoreJSEvents.registerPotionBrewing((event) => {
+	event.removeByPotion(null, "mowziesmobs:naga_fang", null)
 })
