746fd5c703f6422d4e91f97027391b39a23f2b23
pieman492 <rauljrrauljr@gmail.com>
2024-09-19 02:06:54 -0500

    Oops typo :3

--- kubejs/startup_scripts/voidscape_tools.js
+++ kubejs/startup_scripts/voidscape_tools.js
@@ -1,50 +1,50 @@
 ItemEvents.modification(event => {
-    event.modification('voidscape:voidic_crystal_sword', item => {
+    event.modify('voidscape:voidic_crystal_sword', item => {
         item.attackDamage = 7    
     })
-    event.modification('voidscape:voidic_crystal_axe', item => {
+    event.modify('voidscape:voidic_crystal_axe', item => {
         item.attackDamage = 8
     })
-    event.modification('voidscape:voidic_crystal_pickaxe', item => {
+    event.modify('voidscape:voidic_crystal_pickaxe', item => {
         item.attackDamage = 5    
     })
-    event.modification('voidscape:corrupt_sword', item => {
+    event.modify('voidscape:corrupt_sword', item => {
         item.attackDamage = 6 
     })
-    event.modification('voidscape:corrupt_axe', item => {
+    event.modify('voidscape:corrupt_axe', item => {
         item.attackDamage = 7 
     })
-    event.modification('voidscape:titanite_sword', item => {
+    event.modify('voidscape:titanite_sword', item => {
         item.attackDamage = 5 
     })
-    event.modification('voidscape:titanite_axe', item => {
+    event.modify('voidscape:titanite_axe', item => {
         item.attackDamage = 6 
     })
-    event.modification('voidscape:titanite_pickaxe', item => {
+    event.modify('voidscape:titanite_pickaxe', item => {
         item.attackDamage = 4 
     })
-    event.modification('voidscape:ichor_sword', item => {
+    event.modify('voidscape:ichor_sword', item => {
         item.attackDamage = 4 
     })
-    event.modification('voidscape:ichor_axe', item => {
+    event.modify('voidscape:ichor_axe', item => {
         item.attackDamage = 5 
     })
-    event.modification('voidscape:ichor_pickaxe', item => {
+    event.modify('voidscape:ichor_pickaxe', item => {
         item.attackDamage = 3 
     })
-    event.modification('voidscape:astral_sword', item => {
+    event.modify('voidscape:astral_sword', item => {
         item.attackDamage = 3 
     })
-    event.modification('voidscape:astral_axe', item => {
+    event.modify('voidscape:astral_axe', item => {
         item.attackDamage = 4 
     })
-    event.modification('voidscape:astral_pickaxe', item => {
+    event.modify('voidscape:astral_pickaxe', item => {
         item.attackDamage = 2 
     })
-    event.modification('voidscape:astral_shovel', item => {
+    event.modify('voidscape:astral_shovel', item => {
         item.attackDamage = 3.5 
     })
-    event.modification('voidscape:titanite_hoe', item => {
+    event.modify('voidscape:titanite_hoe', item => {
         item.attackDamage = 1 
     })
 
