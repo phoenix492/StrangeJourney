037fbaaa6d8d5a0fe6b82527f9951b3ca429617f
pieman492 <rauljrrauljr@gmail.com>
2024-09-21 01:56:01 -0500

    Closes #20 for real this time

--- kubejs/startup_scripts/voidscape_armor.js
+++ kubejs/startup_scripts/voidscape_armor.js
@@ -10,14 +10,14 @@ ItemEvents.modification(event => {
     // Functions to quickly set armor toughness and KB resistance
     function matchNetherite(i) {
         event.modify( i, item => {
-            item.armorToughness = 3 
-            item.armorKnockbackResistance = 0.1
+            item.setArmorToughness(3)
+            item.setArmorKnockbackResistance(0.1)
         })
     }
     function netheritePlus1(i) {
         event.modify( i, item => {
-            item.armorToughness = 4
-            item.armorKnockbackResistance = 0.15
+            item.setArmorToughness(4)
+            item.setArmorKnockbackResistance(0.15)
         })
     }
     
@@ -36,31 +36,31 @@ ItemEvents.modification(event => {
     // Now the actual armor values, which is different for each piece
     matchNetheriteArmorTiers.forEach(tier => {
         event.modify(tier + '_helmet', item => {
-            item.armorProtection = 3
+            item.setArmorProtection(3)
         })
         event.modify(tier + '_boots', item => {
-            item.armorProtection = 3
+            item.setArmorProtection(3)
         })
         event.modify(tier + '_chest', item => {
-            item.armorProtection = 8
+            item.setArmorProtection(8)
         })
         event.modify(tier + '_legs', item => {
-            item.armorProtection = 6
+            item.setArmorProtection(6)
         })
     })
 
     // Now Astral
     event.modify('voidscape:astral_helmet', item => {
-        item.armorProtection = 4
+        item.setArmorProtection(4)
     })
     event.modify('voidscape:astral_chest', item => {
-        item.armorProtection = 9
+        item.setArmorProtection(9)
     })
     event.modify('voidscape:astral_legs', item => {
-        item.armorProtection = 7
+        item.setArmorProtection(7)
     })
     event.modify('voidscape:astral_helm', item => {
-        item.armorProtection = 4
+        item.setArmorProtection(4)
     })
 
 })
--- kubejs/startup_scripts/voidscape_tools.js
+++ kubejs/startup_scripts/voidscape_tools.js
@@ -1,51 +1,51 @@
 ItemEvents.modification(event => {
     event.modify('voidscape:voidic_crystal_sword', item => {
-        item.attackDamage = 7    
+        item.setAttackDamage(7)
     })
     event.modify('voidscape:voidic_crystal_axe', item => {
-        item.attackDamage = 8
+        item.setAttackDamage(8)
     })
     event.modify('voidscape:voidic_crystal_pickaxe', item => {
-        item.attackDamage = 5    
+        item.setAttackDamage(5)
     })
     event.modify('voidscape:corrupt_sword', item => {
-        item.attackDamage = 6 
+        item.setAttackDamage(6) 
     })
     event.modify('voidscape:corrupt_axe', item => {
-        item.attackDamage = 7 
+        item.setAttackDamage(7) 
     })
     event.modify('voidscape:titanite_sword', item => {
-        item.attackDamage = 5 
+        item.setAttackDamage(5) 
     })
     event.modify('voidscape:titanite_axe', item => {
-        item.attackDamage = 6 
+        item.setAttackDamage(6) 
     })
     event.modify('voidscape:titanite_pickaxe', item => {
-        item.attackDamage = 4 
+        item.setAttackDamage(4) 
     })
     event.modify('voidscape:ichor_sword', item => {
-        item.attackDamage = 4 
+        item.setAttackDamage(4) 
     })
     event.modify('voidscape:ichor_axe', item => {
-        item.attackDamage = 5 
+        item.setAttackDamage(5) 
     })
     event.modify('voidscape:ichor_pickaxe', item => {
-        item.attackDamage = 3 
+        item.setAttackDamage(3) 
     })
     event.modify('voidscape:astral_sword', item => {
-        item.attackDamage = 3 
+        item.setAttackDamage(3) 
     })
     event.modify('voidscape:astral_axe', item => {
-        item.attackDamage = 4 
+        item.setAttackDamage(4) 
     })
     event.modify('voidscape:astral_pickaxe', item => {
-        item.attackDamage = 2 
+        item.setAttackDamage(2) 
     })
     event.modify('voidscape:astral_shovel', item => {
-        item.attackDamage = 3.5 
+        item.setAttackDamage(3.5)
     })
     event.modify('voidscape:titanite_hoe', item => {
-        item.attackDamage = 1 
+        item.setAttackDamage(1)
     })
 
 })
