e2790351bf635cf13c5915b26925cd19a9fd8e39
pieman492 <rauljrrauljr@gmail.com>
2024-09-21 00:49:45 -0500

    Unifies TFMG materials, closes #89

---
+++ kubejs/server_scripts/tfmg.js
@@ -0,0 +1,7 @@
+ServerEvents.tags('item', event => {
+    event.add('forge:bitumen', 'tfmg:bitumen')
+    event.add('forge:slag', 'tfmg:slag')
+    event.add('forge:dusts/sulfur', 'tfmg:sulfur_dust')
+    event.add('forge:dusts/saltpeter', 'tfmg:nitrate_dust')
+    event.add('forge:coal_coke', 'tfmg:coal_coke')
+})
