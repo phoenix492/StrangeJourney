64848f791bec47e51efede86a84c98874a1c782b
pieman492 <rauljrrauljr@gmail.com>
2025-07-22 02:15:47 -0500

    Add forge:dusts/salt tag to spelunkery salt

--- kubejs/server_scripts/spelunkery.js
+++ kubejs/server_scripts/spelunkery.js
@@ -1,4 +1,5 @@
 ServerEvents.tags( 'item', event => {
     event.add('forge:dusts/saltpeter', 'spelunkery:saltpeter')
     event.add('forge:dusts/sulfur', 'spelunkery:sulfur')
+    event.add('forge:dusts/salt', 'spelunkery:salt')
 })
