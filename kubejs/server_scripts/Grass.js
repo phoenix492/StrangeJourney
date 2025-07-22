a130d9821ee9bd720d8730fd57bb2fd8796b1ea1
pieman492 <rauljrrauljr@gmail.com>
2025-07-22 03:36:43 -0500

    Rename Grass.js, remove AA seeds from grass

--- kubejs/server_scripts/Grass.js
+++ kubejs/server_scripts/Grass.js
@@ -1,7 +1,10 @@
 let removedSeeds = [
     "occultism:datura_seeds",
     "immersiveengineering:seed",
-    "hexerei:sage_seed"
+    "hexerei:sage_seed",
+    "actuallyadditions:rice_seeds",
+    "actuallyadditions:canola_seeds",
+    "actuallyadditions:flax_seeds"
 ]
 LootJS.modifiers(event => {
     removedSeeds.forEach(seed => {
