7707f00106100da3e9d89f49a78e222d27d1e90d
pieman492 <rauljrrauljr@gmail.com>
2025-08-03 08:57:52 -0500

    Oops

--- VariantusDeletus/CompileVariants.py
+++ VariantusDeletus/CompileVariants.py
@@ -4,79 +4,79 @@ def transform_quark_mc_carpets(variant_id):
     variant_id = variant_id.strip()
     _,item = variant_id.split(':')
     woodtype = item.removesuffix("_leaf_carpet")
-    return f"unique,minecraft:{woodtype}_leaves,{variant_id},"
+    return f"unique,minecraft:{woodtype}_leaves,{variant_id}"
 
 def transform_quark_mc_hedges(variant_id):
     variant_id = variant_id.strip()
     _,item = variant_id.split(':')
     woodtype = item.removesuffix("_hedge")
-    return f"unique2,minecraft:{woodtype}_leaves,{variant_id},"
+    return f"unique2,minecraft:{woodtype}_leaves,{variant_id}"
 
 def transform_quark_carpets(variant_id):
     variant_id = variant_id.strip()
     modid,item = variant_id.split(':')
     woodtype = item.removesuffix("_leaf_carpet")
-    return f"unique,{modid}:{woodtype}_leaves,{variant_id},"
+    return f"unique,{modid}:{woodtype}_leaves,{variant_id}"
 
 def transform_quark_hedges(variant_id):
     variant_id = variant_id.strip()
     modid,item = variant_id.split(':')
     woodtype = item.removesuffix("_hedge")
-    return f"unique2,{modid}:{woodtype}_leaves,{variant_id},"
+    return f"unique2,{modid}:{woodtype}_leaves,{variant_id}"
 
 def transform_everycomp_carpets(variant_id):
     variant_id = variant_id.strip()
     _, modid, block = variant_id.removeprefix("everycomp:").split('/')
     block = block.removesuffix("_leaf_carpet") 
-    return f"unique,{modid}:{block}_leaves,{variant_id},"
+    return f"unique,{modid}:{block}_leaves,{variant_id}"
 
 def transform_everycomp_hedges(variant_id):
     variant_id = variant_id.strip()
     _, modid, block = variant_id.removeprefix("everycomp:").split('/')
     block = block.removesuffix("_hedge") 
-    return f"unique2,{modid}:{block}_leaves,{variant_id},"
+    return f"unique2,{modid}:{block}_leaves,{variant_id}"
 
 def transform_everycomp_hollow_logs(variant_id):
     variant_id = variant_id.strip()
     _, modid, block = variant_id.removeprefix("everycomp:").split('/')
     block = block.removeprefix("hollow_") 
-    return f"unique2,{modid}:{block},{variant_id},"
+    return f"unique2,{modid}:{block},{variant_id}"
 
 def transform_everycomp_posts(variant_id):
     variant_id = variant_id.strip()
     _, modid, block = variant_id.removeprefix("everycomp:").split('/')
     block = block.removesuffix("_post") 
-    return f"unique,{modid}:{block}_log,{variant_id},"
+    return f"unique,{modid}:{block}_log,{variant_id}"
 
 def transform_everycomp_boardwalks(variant_id):
     variant_id = variant_id.strip()
     _, modid, block = variant_id.removeprefix("everycomp:").split('/')
     block = block.removesuffix("_boardwalk") 
-    return f"unique3,{modid}:{block}_planks,{variant_id},"
+    return f"unique3,{modid}:{block}_planks,{variant_id}"
 
 def transform_everycomp_banisters(variant_id):
     variant_id = variant_id.strip()
     _, modid, block = variant_id.removeprefix("everycomp:").split('/')
     block = block.removesuffix("_banister") 
-    return f"unique2,{modid}:{block}_planks,{variant_id},"
+    return f"unique2,{modid}:{block}_planks,{variant_id}"
 
 def transform_everycomp_log_fences(variant_id):
     variant_id = variant_id.strip()
     _, modid, block = variant_id.removeprefix("everycomp:").split('/')
     block = block.removeprefix("fence_log_") 
-    return f"fence,{modid}:{block}_log,{variant_id},"
+    return f"fence,{modid}:{block}_log,{variant_id}"
 
 def transform_everycomp_log_walls(variant_id):
     variant_id = variant_id.strip()
     _, modid, block = variant_id.removeprefix("everycomp:").split('/')
     block = block.removeprefix("wall_stripped_").removeprefix("wall_")
-    return f"wall,{modid}:{block}_log,{variant_id},"
+    return f"wall,{modid}:{block}_log,{variant_id}"
 
 def transform_everycomp_plank_walls(variant_id):
     variant_id = variant_id.strip()
     _, modid, block = variant_id.removeprefix("everycomp:").split('/')
     block = block.removeprefix("wall_") 
-    return f"wall,{modid}:{block},{variant_id},"
+    return f"wall,{modid}:{block},{variant_id}"
 
 def transform_noop(variant_id):
     variant_id = variant_id.strip()
