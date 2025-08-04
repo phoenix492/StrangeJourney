f793d6592f5130f2cc7f047a64e6568fec242c37
pieman492 <rauljrrauljr@gmail.com>
2025-08-03 20:35:00 -0500

    migrate kubejs to datapack

--- kubejs/server_scripts/chipped.js
+++
@@ -1,9 +0,0 @@
-ServerEvents.tags('item', event => {
-    event.add('strangejourney:chipped_table', 'chipped:botanist_workbench')
-    event.add('strangejourney:chipped_table', 'chipped:glassblower')
-    event.add('strangejourney:chipped_table', 'chipped:carpenters_table')
-    event.add('strangejourney:chipped_table', 'chipped:loom_table')
-    event.add('strangejourney:chipped_table', 'chipped:mason_table')
-    event.add('strangejourney:chipped_table', 'chipped:alchemy_bench')
-    event.add('strangejourney:chipped_table', 'chipped:tinkering_table')
-})
