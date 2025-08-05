433471c23456df8150333d3e72d390beab41e217
pieman492 <rauljrrauljr@gmail.com>
2025-08-05 17:40:08 -0500

    Switch back to Face slot

--- kubejs/startup_scripts/SilphScopeItem.js
+++ kubejs/startup_scripts/SilphScopeItem.js
@@ -2,7 +2,7 @@ StartupEvents.registry('item', event=> {
 	event.create('silph_scope')
 			 .maxStackSize(1)
 			 .tooltip('\u00a7aAllows you to enter the World of Pokemon!')
-			 .tag('curios:head')
+			 .tag('curios:face')
 			 .translationKey('item.strangejourney.silph_scope')
 			 .displayName('Silph Scope')
 })
