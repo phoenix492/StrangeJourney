852d2ebaccbbac065b5e69377e6c252734aae262
pieman492 <rauljrrauljr@gmail.com>
2025-08-05 17:03:27 -0500

    fix typo with limbo tp

--- kubejs/server_scripts/DivinePunishment.js
+++ kubejs/server_scripts/DivinePunishment.js
@@ -2,7 +2,7 @@ EntityEvents.hurt(event => {
 	/* The goal is to return as fast as possible if our conditions aren't matched,
 	 * so we aren't wasting execution time. 
 	 */ 
-	if (!event.source.type().msgId() !== "outOfWorld" || event.entity.isPlayer() || event.entity.isFake()) {
+	if (event.source.type().msgId() !== "outOfWorld" || !event.entity.isPlayer() || event.entity.isFake()) {
 		return
 	}
 	event.server.runCommandSilent(`execute in dimdoors:limbo run tp ${event.entity.getUsername()} ~ 500 ~`)
