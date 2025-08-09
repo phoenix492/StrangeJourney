bfb5268568d4544f72dda52011906656b3c77f3b
pieman492 <rauljrrauljr@gmail.com>
2025-08-09 13:57:40 -0500

    Typo fixes

--- kubejs/server_scripts/Gamerules.js
+++ kubejs/server_scripts/Gamerules.js
@@ -1,7 +1,7 @@
 ServerEvents.loaded(event => {
     event.server.gameRules.set("doTraderSpawning", false)
     event.server.gameRules.set("playersSleepingPercentage", 51)
-		event.server.gameRules.set("announce_advancements", false)
+		event.server.gameRules.set("announceAdvancements", false)
 		event.server.gameRules.set("commandBlockOutput", false)
-		event.server.gameRules.set("max_entity_cramming", 48)
+		event.server.gameRules.set("maxEntityCramming", 48)
 })
