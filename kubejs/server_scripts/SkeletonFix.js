36b5b0dba55d77b99be535d257e73d26aed9a3c5
pieman492 <rauljrrauljr@gmail.com>
2025-08-10 10:13:44 -0500

    MEMEMEMEMEMEME

--- kubejs/server_scripts/SkeletonFix.js
+++ kubejs/server_scripts/SkeletonFix.js
@@ -1,6 +1,6 @@
 // Hacky workaround for Caverns & Chasms stupid, undocumented, and unconfigurable change to skeleton health.
 EntityEvents.spawned(event => {
-	if(event.entity.type === "minecraft:skeleton" && event.entity.getAttributeValue('minecraft:generic.max_health') == 10) {
+	if(event.entity.type === 'minecraft:skeleton' && event.entity.getAttributeValue('minecraft:generic.max_health') == 10) {
 		event.entity.setAttributeBaseValue('minecraft:generic.max_health', 20)
 		event.entity.health = event.entity.maxHealth
 	}
