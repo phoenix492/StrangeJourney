04b6b8aa2f998fd333a822c2b899e8297804029c
pieman492 <rauljrrauljr@gmail.com>
2025-08-13 01:46:44 -0500

    Utility script to generate names from colors

---
+++ DevScripts/OutputColors.py
@@ -0,0 +1,24 @@
+import json
+import sys
+
+colors = [ 
+    "white",
+    "light_gray",
+    "gray",
+    "black",
+    "brown",
+    "red",
+    "orange",
+    "yellow",
+    "lime",
+    "green",
+    "cyan",
+    "light_blue",
+    "blue",
+    "purple",
+    "magenta",
+    "pink"
+]
+
+for arrayColor in colors:
+    print(sys.argv[1].format(color=arrayColor))
