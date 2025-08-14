b0f37806d65cc6425a064bd3aa9bcd0d82ddb7df
pieman492 <rauljrrauljr@gmail.com>
2025-08-14 08:42:13 -0500

    Extra comment

--- kubejs/server_scripts/DivinePunishment.js
+++ kubejs/server_scripts/DivinePunishment.js
@@ -5,6 +5,8 @@ EntityEvents.hurt(event => {
 	if (event.source.type().msgId() !== "outOfWorld" || !event.entity.isPlayer() || event.entity.isFake()) {
 		return
 	}
+	// I'm not the biggest fan of this but this whole sequence along with the 
+	// teleport is basically how Enigmatica does their void TP thing in Nomadic Tents
 	event.server.runCommandSilent(`execute in dimdoors:limbo run tp ${event.entity.getUsername()} ~ 500 ~`)
 	event.cancel()
 })
